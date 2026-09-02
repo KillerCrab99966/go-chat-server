6df7822c0d0343758ecfc048f92df9c1dc9661d9
Aston Lawrence <aston.lawrence.nz@gmail.com>
2026-09-02 23:02:00 +1200

    Add metadata to connections and improve loggig

--- ws.go
+++ ws.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
-	"time"
 
 	"github.com/coder/websocket"
 )
+
+var clientCount = 0
+
+type client struct {
+	c  *websocket.Conn
+	id int
+}
+
+func newClient(c *websocket.Conn, id int) *client {
+	return &client{c, id}
+}
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 	c, err := websocket.Accept(w, r, nil)
@@ -16,13 +28,32 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer c.CloseNow()
 
-	ctx, cancel := context.WithTimeout(r.Context(), time.Minute)
-	defer cancel()
+	cl := newClient(c, clientCount)
+	clientCount++
 
-	_, bytes, err := c.Read(ctx)
-	if err != nil {
-		fmt.Println(err)
+	fmt.Printf("Client [%d] connected\n", cl.id)
+
+	ctx := context.Background()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			_, bytes, err := c.Read(ctx)
+			if err != nil {
+				// Normal disconnect
+				if websocket.CloseStatus(err) == websocket.StatusNormalClosure ||
+					websocket.CloseStatus(err) == websocket.StatusGoingAway ||
+					errors.Is(err, io.EOF) {
+					fmt.Printf("Client [%d] disconnected\n", cl.id)
+					return
+				}
+
+				fmt.Println(err)
+			}
+
+			fmt.Printf("[%d] %v\n", cl.id, string(bytes))
+		}
 	}
-
-	fmt.Println(string(bytes))
 }
